716bf044b9ba00c5b306980302a940b2c18108a2
Andy <andyaldodharmawan@gmail.com>
2023-08-09 15:39:49 +0700

    Svc/golang (#1)
    * go mod init backend-go
    
    * Get boilerplate code from gobyexample
    
    * Create CI pipeline for golang svc
    
    * add path to dockerfile
    
    * Upload and download golang binary between steps
    
    * Try to deploy to minikube
    
    * set CGO_ENABLED=0
    
    to avoid dynamic link issue when running go binary
    
    * Edit golang server
    
    - Log before http.ListenAndServe
    - listen to port 3000
    
    * Fix helm chart relative path
    
    * Add / handler for k8s health check
    
    * Set default port to 80
    
    * helm chart init http
    
    * add timeout to helm deploy command
    
    * Test the endpoint after deployed to minikube
    
    * Set service.type to NodePort
    
    * Add new line after echo
    
    * Add -s to curl command
    
    * Use gorilla mux
    
    * Add comments
    
    * Add basic test
    
    * Set overall target to 0 for testing purposes
    
    * Relocate comment
    
    * Update .gitignore
    
    * Use upload/download-artifact@v3

---
+++ services/golang/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "pong\n")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
+func main() {
+
+	// Routes
+	rtr := mux.NewRouter()
+
+	rtr.HandleFunc("/", hello) // This route is added for k8s liveness probe
+	rtr.HandleFunc("/ping", hello)
+	rtr.HandleFunc("/headers", headers)
+	http.Handle("/", rtr)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "80"
+	}
+
+	log.Println("Listening...")
+	err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	if err != nil {
+		log.Println(err)
+		os.Exit(255)
+	}
+}
